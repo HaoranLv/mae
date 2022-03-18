f724ab53e2149703701c1283d0ef7ca6106d6dc5
HaoranLv <49785898+HaoranLv@users.noreply.github.com>
2022-03-18 16:49:43 +0800

    1

--- util/misc.py
+++ util/misc.py
@@ -292,46 +292,24 @@ def get_grad_norm_(parameters, norm_type: float = 2.0) -> torch.Tensor:
     return total_norm
 
 
-def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler, save_last_only=False):
+def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
     output_dir = Path(args.output_dir)
     epoch_name = str(epoch)
-    if save_last_only:
-        try:
-            os.remove(os.path.join(args.output_dir,"checkpoint.pth"))
-        except:
-            pass
-        if loss_scaler is not None:
-            checkpoint_paths = [output_dir / ('checkpoint.pth')]
-            for checkpoint_path in checkpoint_paths:
-                to_save = {
-                    'model': model_without_ddp.state_dict(),
-                    'optimizer': optimizer.state_dict(),
-                    'epoch': epoch,
-                    'scaler': loss_scaler.state_dict(),
-                    'args': args,
-                }
+    if loss_scaler is not None:
+        checkpoint_paths = [output_dir / ('checkpoint-%s.pth' % epoch_name)]
+        for checkpoint_path in checkpoint_paths:
+            to_save = {
+                'model': model_without_ddp.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'epoch': epoch,
+                'scaler': loss_scaler.state_dict(),
+                'args': args,
+            }
 
-                save_on_master(to_save, checkpoint_path)
-        else:
-            client_state = {'epoch': epoch}
-            model.save_checkpoint(save_dir=args.output_dir, tag="checkpoint", client_state=client_state)
+            save_on_master(to_save, checkpoint_path)
     else:
-        if loss_scaler is not None:
-            checkpoint_paths = [output_dir / ('checkpoint-%s.pth' % epoch_name)]
-            for checkpoint_path in checkpoint_paths:
-                to_save = {
-                    'model': model_without_ddp.state_dict(),
-                    'optimizer': optimizer.state_dict(),
-                    'epoch': epoch,
-                    'scaler': loss_scaler.state_dict(),
-                    'args': args,
-                }
-
-                save_on_master(to_save, checkpoint_path)
-        else:
-            client_state = {'epoch': epoch}
-            model.save_checkpoint(save_dir=args.output_dir, tag="checkpoint-%s" % epoch_name, client_state=client_state)
-
+        client_state = {'epoch': epoch}
+        model.save_checkpoint(save_dir=args.output_dir, tag="checkpoint-%s" % epoch_name, client_state=client_state)
 
 
 def load_model(args, model_without_ddp, optimizer, loss_scaler):
